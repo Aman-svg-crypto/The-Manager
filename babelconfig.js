e5075cc22401e728870a0d80e692839ed556e5d1
Aman pandey <bambambholeyarun@gmail.com>
2026-03-21 10:09:56 +0530

    Fix formatting in babelconfig.js

--- babelconfig.js
+++ babelconfig.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', // यह लाइन जोड़ना अनिवार्य है
+      'react-native-reanimated/plugin',
     ],
   };
 };
